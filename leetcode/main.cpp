4009022702d7f8bbbe67f36eb285fb4f62477a81
hazyparker_linux <2559272883@qq.com>
2022-04-14 20:05:11 +0800

    add question1(unsolved)

---
+++ leetcode/main.cpp
@@ -0,0 +1,23 @@
+//
+// this file is created by hazyparker on 2022/4/14
+// this doc is mainly on code practice from https://leetcode-cn.com/problemset/all/
+//
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+void main_advanced() {
+    cout << "please choose question number:";
+    string question_id;
+    cin >> question_id;
+
+#define DEF_TEST(f) if ( work_name == #f ) return f();
+    //DEF_TEST(function1_1);
+}
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
---
+++ leetcode/src/question1.h
@@ -0,0 +1,8 @@
+//
+// Created by hazyparker on 2022/4/14.
+//
+
+#ifndef LEETCODE_QUESTION1_H
+#define LEETCODE_QUESTION1_H
+
+#endif //LEETCODE_QUESTION1_H
